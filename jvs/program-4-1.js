50d4c8253cb6d07e2b6f411b47d20073f9f28cdb
ortizal <alexis.ortiz81@outlook.com>
2019-05-17 21:56:27 -0500

    updated home page

---
+++ jvs/program-4-1.js
@@ -0,0 +1,9 @@
+function intname(){
+//input
+let x = document.getElementById('name').value;
+//processing
+//let y = name;
+//output
+document.getElementById('output').innerHTML = 'Behold, thou art ' + x  + ', and I have spoken unto thee because of thy desires; therefore atreasure up these words in thy heart. Be faithful and bdiligent in keeping the commandments of God, and I will encircle thee in the arms of my clove.'+''+ 
+'D y C 20:6';
+}
