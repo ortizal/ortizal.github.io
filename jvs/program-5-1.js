d0c09b43df98a9845c0a1701a9345508fe09ae85
ortizal <alexis.ortiz81@outlook.com>
2019-05-24 18:03:02 -0500

    updated home page

---
+++ jvs/program-5-1.js
@@ -0,0 +1,14 @@
+// Get Current Date
+let today = new Date(); // new Date object
+// now concatenate formatted output
+let date = (today.getMonth()+1) + " / " +  today.getDate() + " / " + today.getFullYear();
+document.getElementById('currentdate').innerHTML = date;
+
+function VolumeCylinder(){
+  let r= parseFloat(document.getElementById("radio").value);
+  let h= parseFloat(document.getElementById("height").value);
+  let pi = 3.1415926535;
+  if (r > 0 && h >0 ){ let volume = pi * r * r * h;  
+  document.getElementById('output').innerHTML = volume ;  }
+   else {alert ("WRONG, CHECK THAT THE VALUES ARE POSITIVE");}
+  }
