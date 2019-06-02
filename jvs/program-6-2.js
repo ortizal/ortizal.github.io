8f1990e6fbab70b7ecf57fddc85088190b47ef51
ortizal <alexis.ortiz81@outlook.com>
2019-06-02 00:24:10 -0500

    updated home page

--- jvs/program-6-2.js
+++ jvs/program-6-2.js
@@ -5,25 +5,24 @@ document.getElementById('currentdate').innerHTML = date;
 function randomNumbers(max, min) {
 	min = Math.ceil(0);
 	max = Math.floor(101);
- let firstNumber = Math.floor(Math.random() * (max - min)) + min;
+    let firstNumber = Math.floor(Math.random() * (max - min)) + min;
 	let secondNumber = Math.floor(Math.random() * (max - min)) + min;
-document.getElementById("firstNumber").value = firstNumber;
-document.getElementById("secondNumber").value = secondNumber;
-document.getElementById("output").innerHTML = output;
-let answer = ""
-document.getElementById("answer").value = answer;
-	
+	document.getElementById("firstNumber").value = firstNumber;
+	document.getElementById("secondNumber").value = secondNumber;
+	document.getElementById("output").innerHTML = output;
+	let output ="Fill the box, add the numbers "
+	let answer = ""
+	document.getElementById("answer").value = answer;
 }
-function addNumbers() {
+	function addNumbers() {
 	let answer =	parseInt(document.getElementById("answer").value);
 	let n1 = parseInt(document.getElementById("firstNumber").value);
 	let n2 = parseInt(document.getElementById("secondNumber").value);
 	if (n1 + n2 == answer) {		
 		let output = "Correct! Good job."
 		document.getElementById("output").innerHTML = output;
-		} else {
+		} 
+	else {
 		let output = "Sorry. That is incorrect."
-	document.getElementById("output").innerHTML = output;
-    }
-
-}
+		document.getElementById("output").innerHTML = output;
+    	}}
