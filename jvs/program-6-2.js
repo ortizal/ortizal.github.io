65047c6b9d3c105030a827876b62815abb6afbbd
ortizal <alexis.ortiz81@outlook.com>
2019-06-02 00:17:39 -0500

    updated home page

--- jvs/program-6-2.js
+++ jvs/program-6-2.js
@@ -1,21 +1,20 @@
+let today = new Date ();
+let date = today.getMonth()+1 + '/' + today.getDate() + '/' + today.getFullYear();
+document.getElementById('currentdate').innerHTML = date;
 
-
-function generateQues(max, min) {
-	min = Math.ceil(1);
+function randomNumbers(max, min) {
+	min = Math.ceil(0);
 	max = Math.floor(101);
  let firstNumber = Math.floor(Math.random() * (max - min)) + min;
 	let secondNumber = Math.floor(Math.random() * (max - min)) + min;
-//OUTPUT 
 document.getElementById("firstNumber").value = firstNumber;
 document.getElementById("secondNumber").value = secondNumber;
-let output = "Good Luck!"
 document.getElementById("output").innerHTML = output;
 let answer = ""
 document.getElementById("answer").value = answer;
 	
 }
-//PROCESS
-function checkSum() {
+function addNumbers() {
 	let answer =	parseInt(document.getElementById("answer").value);
 	let n1 = parseInt(document.getElementById("firstNumber").value);
 	let n2 = parseInt(document.getElementById("secondNumber").value);
