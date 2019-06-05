ccf77c82b3b90c631837f8abd24fff3048b1d578
ortizal <alexis.ortiz81@outlook.com>
2019-06-05 17:18:48 -0500

    updated home page

--- jvs/program-7-2.js
+++ jvs/program-7-2.js
@@ -6,15 +6,18 @@ function subtotal(){
   let dayOfWeek = new Date().getDay(); 
   //document.getElementById('output').innerHTML = dayOfWeek;
   let subt = document.getElementById("subt").value;
-   if (dayOfWeek == 7 || dayOfWeek == 4){
+   if (dayOfWeek == 3 || dayOfWeek == 4){
     if (subt >= 50){
         let total1 = (subt * 10)/100;
-        let total = subt - total1;
+       let total = subt - total1;
         document.getElementById('output').innerHTML = total;
-    } else { document.getElementById('output').innerHTML = "es menor a 50";}
+    } else if (subt <50 ){ 
+       let total1 = (subt * 6)/100;
+       let total = subt - total1;
+      document.getElementById('output').innerHTML = "Your purchase is less than $50, your discount is 6%. Total is: " + total;}
    
   }
   else {
-    document.getElementById('output').innerHTML = 'wrong';
+    document.getElementById('output').innerHTML = 'Today is not discount day. Total is:' + subt;
   }
 }
