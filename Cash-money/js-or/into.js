69b21098d9b7175b680410b0e853c42bf5cc3354
ortizal <alexis.ortiz81@outlook.com>
2019-06-18 20:37:18 -0500

    updated home page

--- Cash-money/js-or/into.js
+++ Cash-money/js-or/into.js
@@ -7,13 +7,12 @@ let usname=document.getElementById("usname").value;
 let usname1= "ortiz";
 let pasword=document.getElementById("pasword").value;  
 let pasword1="tulcan";
-let menu=document.getElementById("menulu").value;
 if (usname==usname1 && pasword==pasword1){
-  window.alert('Bienvenido');
-  window.open(url,menu);
+  
+  location.href="menu.html";
+
 }
 else{
   window.alert('Error, Has ingresado mal el usuario o la contraseña. Intenta nuevamente.')
 }
-
 }
