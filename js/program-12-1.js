d2538d8d220ad35ef829d0264e4a3a764550fa01
ortizal <alexis.ortiz81@outlook.com>
2019-07-13 16:45:30 -0500

    updated home page

---
+++ js/program-12-1.js
@@ -0,0 +1,10 @@
+let today = new Date();
+let date =
+  today.getMonth() + 1 + " / " + today.getDate() + " / " + today.getFullYear();
+document.getElementById("currentdate").innerHTML = date;
+function addEnds() {
+    let list = [56, 25, 14, 52];
+    let sum = list[0] + list[list.length -1];
+    document.getElementById("output").innerHTML =
+      "The add of the first and the last number is " + sum;
+  }
