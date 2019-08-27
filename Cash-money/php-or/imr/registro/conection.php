28ccc2890ea86010a58be8c221bac6879b596dbd
ortizal.github.io <alexis.ortiz81@outlook.com>
2019-08-27 15:03:48 -0500

    updated home page

--- Cash-money/php-or/imr/registro/conection.php
+++ Cash-money/php-or/imr/registro/conection.php
@@ -3,7 +3,7 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $db = "contabilidaddb";
-mysql_connect($host,$user,$pass);
+mysql_connect($host, $user, $pass);
 //mysql_set_charset($conection, "utf8");
-mysql_select_db($contabilidaddb);
+mysql_select_db($db);
 ?>
--- Cash-money/php-or/imr/registro/insert.php
+++ Cash-money/php-or/imr/registro/insert.php
@@ -2,14 +2,22 @@
 $id = $_POST['txt_id'];
 $nombre = $_POST['txt_nombre'];
 $apellido = $_POST['txt_apellido'];
-$fechaNc = $_POST['text_fechaNc'];
-$email = $_POST['text_email'];
-$telefono = $_POST['text_telefono'];
-$usuario = $_POST['text_usuario'];
-$contraseña = $_POST['text_contraseña'];
-$cedula = $_POST['text_cedula'];
+$fechaNc = $_POST['txt_fechaNc'];
+$email = $_POST['txt_email'];
+$telefono = $_POST['txt_telf'];
+$usuario = $_POST['txt_usr'];
+$contraseña = $_POST['txt_contraseña'];
+$cedula = $_POST['txt_cedula'];
 //include("conection.php");
-// $sql= "INSERT INTO usuario(usr_id,usr_nombre,usr_apellido,usr_fechaNc,usr_email,usr_telefono,usr_usuario,usr_contraseña,usr_cedula) VALUES ('$usr_id','$usr_nombre','$usr_apellido','$usr_fechaNc','$usr_email','$usr_telefono','$usr_usuario','$usr_contraseña','$usr_cedula')"; 
- //myqli_query($conection,$sql);
-echo "<h2> la se paso con exito la informacion </h2>"
+//mysql_query ("insert into usuario(usr_id, usr_nombre, usr_apellido, usr_fechaNc, usr_email, usr_telefono, usr_usuario, usr_contraseña, usr_cedula) values ('$id','$nombre','$apellido','$fechaNc','$email','$telefono','$usuario','$contraseña','$cedula')"); 
+//echo "<h2> la informacion se paso con exito la informacion </h2>";
+echo "el id es:" .$id."</br>";
+echo "el nombre es:" .$nombre."</br>";
+echo "el apellido es:" .$apellido."</br>";
+echo "la fecha es:" .$fechaNc."</br>";
+echo "el e-mail es:" .$email."</br>";
+echo "el telefono es:" .$telefono."</br>";
+echo "el usuario es:" .$usuario."</br>";
+echo "la contraseña es:" .$contraseña."</br>";
+echo "la cedula es:" .$cedula."</br>";
 ?>
