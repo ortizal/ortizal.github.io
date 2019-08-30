d13442e833d2aa5e6e8392f001f585a6289289d8
ortizal.github.io <alexis.ortiz81@outlook.com>
2019-08-29 19:48:36 -0500

    updated home page

--- Cash-money/php-or/imr/registro/insert.php
+++ Cash-money/php-or/imr/registro/insert.php
@@ -7,10 +7,10 @@ $fechaNc = $_POST['txt_fechaNc'];
 $email = $_POST['txt_email'];
 $telefono = $_POST['txt_telf'];
 $usuario = $_POST['txt_usr'];
-$contraseña = $_POST['txt_contraseña'];
+$pasword = $_POST['txt_pasword'];
 $cedula = $_POST['txt_cedula'];
-$res="INSERT INTO usuarios(usr_id, usr_nombres, usr_apellidos, usr_fechaNacimiento, usr_email, usr_telefono, usr_usuario, usr_contraseña, usr_cedula)
- VALUES ('$id','$nombre','$apellido','$fechaNc','$email','$telefono','$usuario','$contraseña','$cedula')"; 
+$res="INSERT INTO usuarios(usr_id, usr_nombres, usr_apellidos, usr_fechaNacimiento, usr_email, usr_telefono, usr_usuario, usr_pasword, usr_cedula)
+ VALUES ('$id','$nombre','$apellido','$fechaNc','$email','$telefono','$usuario','$pasword','$cedula')"; 
 $resultado=mysqli_query($con,$res) or die (mysqli_error($con));
 if ($resultado){
     echo "exitoso";
